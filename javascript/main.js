caef4e06fd5fd5553c9646042734f560cebfef9b
JoseAGonzalezA <jose.a.gonzalez.alcantara@gmail.com>
2021-09-09 02:30:25 +0200

    Reestucturacion JavaScript más eficiente y comprobación de introduccion de nuemro

--- javascript/main.js
+++ javascript/main.js
@@ -6,35 +6,42 @@ const MAX_CHECKS = 4;
 const MIN_CHECKS = 1;
 
 function insertActivity(index){
+    var titleAtivity = "";
+    var numRepetitions = 0;
 
     do{
-        var titleAtivity = prompt("Escribe el nombre de la actividad");
+        titleAtivity = prompt("Escribe el nombre de la actividad");
     }while(titleAtivity == null || titleAtivity == "");
+
         document.getElementById("title-th-" + index).innerText = titleAtivity;
+
     do{     
-        var numRepetitions = parseInt(prompt("Escribe el número de repeticiones diarias entre 1 y 4"));
-        if(numRepetitions < MIN_CHECKS || numRepetitions > MAX_CHECKS){
-            alert("Porfavor asegurese que las repeticiones estan etre 1 y 4");          
+        numRepetitions = prompt("Escribe el número de repeticiones diarias entre 1 y 4");
+        if (typeof numRepetitions === 'string' || numRepetitions instanceof String) {
+            alert("Porfavor asegurese que escribe un número");
         }
-        else{
-            var cellRep = document.getElementById("row-" + index);
-            var cellData = cellRep.children;
-            var cellInnerHTML = "";
-            for (const cell in cellData) {
-                if (cell > 0) {
-                    for (let index = 0; index < numRepetitions; index++) {
-                        if (numRepetitions == 1) {
-                            cellInnerHTML = cellInnerHTML + "<input type='checkbox' class='checkData-1' />";
-                        } else {
-                            cellInnerHTML = cellInnerHTML + "<input type='checkbox' class='checkData' />";
-                        }               
-                    }
-                    cellData[cell].innerHTML = cellInnerHTML;
-                    cellInnerHTML = "";
-                }
+        else
+        {
+            if(numRepetitions < MIN_CHECKS || numRepetitions > MAX_CHECKS){
+                alert("Porfavor asegurese que las repeticiones estan etre 1 y 4");          
             }
         }
     }while(numRepetitions < MIN_CHECKS || numRepetitions > MAX_CHECKS);
+
+        var cellRep = document.getElementById("row-" + index);
+        var cellData = cellRep.children;
+        var cellInnerHTML = "";
+        for (const cell in cellData) {
+            if (cell > 0) {
+                for (let index = 0; index < numRepetitions; index++) {
+                    if (numRepetitions == 1) {
+                        cellInnerHTML = cellInnerHTML + "<input type='checkbox' class='checkData-1' />";
+                    } else {
+                        cellInnerHTML = cellInnerHTML + "<input type='checkbox' class='checkData' />";
+                    }               
+                }
+                cellData[cell].innerHTML = cellInnerHTML;
+                cellInnerHTML = "";
 
 }
 
